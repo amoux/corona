f3bf7133e2c1f478356ced36cf41d23e7ba2e92b
amoux <carlosdeveloper2@gmail.com>
2020-06-19 22:49:28 -0400

    Included tasks from kaggle and updated tunining method for multi-tasks

--- corona_nlp/retrival.py
+++ corona_nlp/retrival.py
@@ -1,10 +1,11 @@
-from typing import List, Optional, Union
+from typing import Dict, List, Optional, Union
 
 import faiss
 import spacy
 from nltk.tokenize import word_tokenize
 from tqdm.auto import tqdm
 
+from .dataset import CORD19Dataset
 from .datatypes import Papers
 from .utils import clean_punctuation, normalize_whitespace
 
@@ -114,40 +115,12 @@ def extract_questions(papers: Papers, min_length=30, sentence_ids=False):
     return questions, ids
 
 
-def tune_dataset_to_tasks(cord19: 'CORD19Dataset',
-                          encoder: 'SentenceTransformer',
-                          tasks: List[str],
-                          minlen: int = 10,
-                          size: int = -1,
-                          goal_size: Optional[int] = None,
-                          k_nn: Optional[int] = None,
-                          show_progress: bool = False) -> List[int]:
-    """Return a sample of ids tuned to an iterable of tasks.
-
-    param: minlen: filter, minimum length of titles.
-    param: size: sample size to use for obtaining the titles.
-    param: goal_size: expected size of a sample. if the expected size
-        is larger; random selected ids will be added to meet fulfillment
-    param: k_nn: k number of neighbors to query against the titles.
-    param: show_progress: wheather to display the progress of encoding.
-    """
+def extract_paper_titles(cord19: CORD19Dataset,
+                         minlen=10, size=-1) -> Dict[int, str]:
+    """Return a map of paper ids to titles extracted from the dataset."""
     sample = cord19.sample(-1)
     if size > -1:
         sample = sample[:size]
-
-    if k_nn is None and goal_size is not None:
-        # find best fit to top k, given number of queries per
-        # centroid (n tasks) in relation to the expected return
-        # size and the total samples available (n paper IDs)
-        ntasks = len(tasks)
-        k_nn = round(goal_size / ntasks) - ntasks % 2
-        maxk = len(sample) - goal_size
-        assert (k_nn * ntasks) <= maxk, (
-            'goal_size is larger than the number of queries possible given'
-            ' the sample size and number of tasks, choose a smaller goal.')
-    else:
-        raise ValueError(
-            'Expected either ``k_nn`` or ``goal_size`` values.')
 
     paper_titles = {}
     for pid in tqdm(sample, desc='titles'):
@@ -157,34 +130,105 @@ def tune_dataset_to_tasks(cord19: 'CORD19Dataset',
         if pid not in paper_titles:
             paper_titles[pid] = title
 
-    paper_index = dict(enumerate(paper_titles.keys()))
+    return paper_titles
+
+
+def tune_dataset_to_tasks(
+        tasks: Union[List[str], List[List[str]]],
+        encoder: 'SentenceTransformer',
+        minlen: Optional[int] = 10,
+        n_size: Optional[int] = -1,
+        cord19: Optional[CORD19Dataset] = None,
+        paper_titles: Optional[Dict[int, str]] = None,
+        target_size: Optional[int] = None,
+        k_nn: Optional[int] = None,
+        show_progress: bool = False) -> Union[List[int], List[List[int]]]:
+    """Return a sample of ids tuned to an iterable of tasks.
+
+    param: tasks (Union[List[str], List[List[str]]]):
+        An iterable of string sequences or a list of iterables of string
+        sequences. Tasks are expected to be in form of text queries.
+        Multiple tasks available in the `cord_nlp.tasks` module.
+    param: minlen (Optional, int):
+        Minimum title length, ignored if paper_titles is not None.
+    param: n_size (Optional, int):
+        Sample size for obtaining the titles, ignored if paper_titles
+        is not None.
+    param: paper_titles (Optional, Dict[int, str]):
+        A mapping of paper ids to its titles. If None, then a
+        ``CORD19Dataset`` instance is expected.
+    param: target_size (Optional, int):
+        Expected size of a sample. If the number of unique IDs is less
+        than the target size; additional ID's from the sample will be added
+        (these are not similar to the tasks) in order to meet the target
+        sample size. Otherwise, no additional ids are added.
+    param: k_nn (Optional, int):
+        Number of k nearest neighbors to query against the titles.
+    param: show_progress (bool):
+        Whether to display the progress of encoding.
+    """
+    if not isinstance(tasks[0], list):
+        tasks = [tasks]
+    if paper_titles is None:
+        if cord19 is not None:
+            paper_titles = extract_paper_titles(cord19, minlen, n_size)
+        else:
+            raise Exception('Expected an ``CORD19Dataset`` instance or '
+                            'a Dict[int, str] ``paper_titles`` mapping.')
+
+    decode = dict(enumerate(paper_titles.keys()))
+    sample = list(paper_titles.keys())
     titles = list(paper_titles.values())
 
-    titles_embed = encoder.encode(titles, show_progress=show_progress)
-    tasks_embed = encoder.encode(tasks, show_progress=show_progress)
+    k_iter = []
+    if k_nn is None and target_size is not None:
+        # find best fit to top k, given number of queries per
+        # centroid (n tasks) in relation to the expected return
+        # size and the total samples available (n paper IDs)
+        for task in tasks:
+            ntasks = len(task)
+            k_nn = round(target_size / ntasks) - ntasks % 2
+            maxk = len(sample) - target_size
+            assert (k_nn * ntasks) <= maxk, (
+                'goal_size is larger than n queries possible given the '
+                'sample size and number of tasks, choose a smaller goal.'
+            )
+            k_iter.append(k_nn)
 
-    ndim = titles_embed.shape[1]
+    db = encoder.encode(titles, 8, show_progress)
+    ndim = db.shape[1]
     index = faiss.IndexFlat(ndim)
-    index.add(titles_embed)
-    _, ids = index.search(tasks_embed, k_nn)
+    index.add(db)
 
-    k_nn_ids = ids.flatten().tolist()
-    gold_ids = sorted(set([paper_index[i] for i in k_nn_ids]))
-    if goal_size is None:
+    gold_ids = []
+    for id in range(len(k_iter)):
+        topk = k_iter[id]
+        w_x = encoder.encode(tasks[id], 8, show_progress)
+        knn = index.search(w_x, topk)[1].flatten().tolist()
+        ids = sorted(set([decode[k] for k in knn]))
+        if target_size is None:
+            gold_ids.append(ids)
+        else:
+            gold_ids.extend(ids)
+
+    if target_size is None:
         return gold_ids
 
+    gold_ids = list(set(gold_ids))
     ntotal = len(gold_ids)
+
     extra_ids = []
-    if ntotal < goal_size:
-        needs = goal_size - ntotal
+    if ntotal < target_size:
+        target = target_size - ntotal
         count = 0
-        for need_id in sample:
-            if need_id in gold_ids:
+        for id in sample:
+            if id in gold_ids:
                 continue
-            if count < needs:
-                extra_ids.append(need_id)
+            if count < target:
+                extra_ids.append(id)
                 count += 1
-        assert len(extra_ids) + len(gold_ids) == goal_size
+        assert len(extra_ids) + ntotal == target_size
 
     gold_ids.extend(extra_ids)
+    gold_ids.sort()
     return gold_ids
